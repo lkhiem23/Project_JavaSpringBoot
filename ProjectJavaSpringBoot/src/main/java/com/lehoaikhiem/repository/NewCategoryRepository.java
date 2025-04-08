b630615216155bb9223abd96fe43512047b63655
lkhiem23 <khiemletn612@gmail.com>
2025-04-08 23:25:26 +0700

    Add NewsCategoryRepository

---
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/repository/NewCategoryRepository.java
@@ -0,0 +1,4 @@
+package com.lehoaikhiem.repository;
+
+public interface NewCategoryRepository {
+}
--- ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/NewsCategoryService.java
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/NewsCategoryService.java
@@ -1,5 +1,9 @@
 package com.lehoaikhiem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NewsCategoryService {
 
 }
