f47e29daf90260be8c1bc523416fd8bb1bb966ba
lkhiem23 <khiemletn612@gmail.com>
2025-04-15 00:00:17 +0700

    TransportMethodService

--- ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/PaymentMethodService.java
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/PaymentMethodService.java
@@ -1,4 +1,10 @@
 package com.lehoaikhiem.service;
 
+import com.lehoaikhiem.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentMethodService {
+
 }
--- ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/TransportMethodService.java
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/TransportMethodService.java
@@ -1,4 +1,15 @@
 package com.lehoaikhiem.service;
 
+import com.lehoaikhiem.dto.TransportMethod.TransportMethodMapper;
+import com.lehoaikhiem.repository.TransportMethodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransportMethodService {
+    @Autowired
+    private TransportMethodRepository transportMethodRepository;
+
+    @Autowired
+    private TransportMethodMapper transportMethodMapper;
 }
