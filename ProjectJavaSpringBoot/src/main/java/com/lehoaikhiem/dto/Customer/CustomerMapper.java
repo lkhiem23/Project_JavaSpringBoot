f41cfaba6c0d0df64f946ffd0eaca32c7e8ac717
lkhiem23 <khiemletn612@gmail.com>
2025-04-14 23:49:36 +0700

    addCustomerMapper

---
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/dto/Customer/CustomerMapper.java
@@ -0,0 +1,14 @@
+package com.lehoaikhiem.dto.Customer;
+
+import com.lehoaikhiem.dto.NewsCategory.NewsCategoryDTO;
+import com.lehoaikhiem.entity.Customer;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    CustomerDTO toCustomerDTO(Customer customer);
+    Customer toNewsCategory(CustomerDTO customerDTO);
+    List<NewsCategoryDTO> toDtoList(List<Customer> entities);
+}
--- ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/CustomerService.java
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/service/CustomerService.java
@@ -1,4 +1,22 @@
 package com.lehoaikhiem.service;
 
+import com.lehoaikhiem.dto.Customer.CustomerDTO;
+import com.lehoaikhiem.dto.Customer.CustomerMapper;
+import com.lehoaikhiem.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    public List<CustomerDTO> findAll{
+        return customerMapper.toDtoList(customerRepository.findAll());
+    }
 }
