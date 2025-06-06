b56d3ee43015c5d7175de512227badee077fb39f
lkhiem23 <khiemletn612@gmail.com>
2025-06-06 23:52:16 +0700

    Add SecurityConfig - Block form Login - Test API Postman

---
+++ ProjectJavaSpringBoot/src/main/java/com/lehoaikhiem/config/SercurityConfig.java
@@ -0,0 +1,32 @@
+package com.lehoaikhiem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SercurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Không khai báo user nào cả, để tắt auto cấu hình user
+        return new InMemoryUserDetailsManager();
+    }
+}
